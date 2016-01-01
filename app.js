6892a54b9d580de640762aed0ab6771c7b2461e5
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-12-31 19:45:27 -0500

    added styling, tested click functionality

---
+++ app.js
@@ -0,0 +1,10 @@
+var yes = document.getElementById("yesbutton");
+var notyet = document.getElementById("notyetbutton");
+
+yes.addEventListener("click", function(){
+  console.log("yes clicked");
+});
+
+notyet.addEventListener("click", function(){
+  console.log("not yet clicked");
+});
