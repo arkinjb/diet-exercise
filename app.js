56b6e7bb4bcd7ba1893020f745e1277244101aa0
JARED ARKIN <ARKINJB@GMAIL.COM>
2016-01-05 11:47:40 -0500

    basic append from image array functionality

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
 
+  var yesPath = "/Users/jba/wdi/diet/images/yes/";
+  var notyetPath = "/Users/jba/wdi/diet/images/notyet/";
+
+  var yesImages = ["brofist.jpg", "linda-gif.gif", "daniel-bryan-gif.gif", "schwing.jpeg", "wooderson.jpg"];
+  var notyetImages = ["legday.jpg", "quickly.jpg", "now-later,jpg", "stop-excuses.png", "no-better-time.png"];
+
   $( "#yesbutton" ).on("click", function(){
     console.log("yes clicked");
+    var src = yesImages[Math.floor(Math.random()*yesImages.length)];
+    $( "#newimage" ).append('<img src="' + yesPath + src + '">');
   });
 
   $( "#notyetbutton" ).on("click", function(){
     console.log("not yet clicked");
+    var src = notyetImages[Math.floor(Math.random()*notyetImages.length)];
+    $( "#newimage" ).append('<img src="' + notyetPath + src + '">');
   });
 
   $(".dropdown-menu li").click(function(){
